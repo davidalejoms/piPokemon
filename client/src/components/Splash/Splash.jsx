56f5b74397044cb9781327621ebda7a0e493dac8
davidalejoms <davidalejoms@gmail.com>
2023-07-21 16:53:46 -0500

    'walking ash'

--- client/src/components/Splash/Splash.jsx
+++ client/src/components/Splash/Splash.jsx
@@ -1,6 +1,6 @@
 import { splash, title, button } from "./Splash.module.css"
 import { useEffect, useState } from "react"
-
+import walking_ash_and_pikachu from '../../assets/walking_ash_and_pikachu.gif'
 const Splash = () => {
 
   const [begin, setBegin] = useState(true)
@@ -10,7 +10,8 @@ const Splash = () => {
   return (
     <>
       <div className={splash} style={begin && {display:'non'}}>
-        <h1 className={title}>Pokemon Api By David Mejia</h1>
+        <h1 className={title}>Pokemon Api <br /> By David Mejia</h1>
+        <img src={walking_ash_and_pikachu} alt="" />
         <button className={button} onClick={beginToogler}>Entrar</button>
       </div>
     </>
