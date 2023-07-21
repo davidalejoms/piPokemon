991588f6244a4e6fa46fd487661f1a9463df32be
davidalejoms <davidalejoms@gmail.com>
2023-07-21 17:05:36 -0500

    'tittles'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,13 @@
-import { useEffect, useState } from "react"
+
 import "./App.css"
 import Splash from "./components/Splash/Splash"
 
 function App() {
-  const [begin, setBegin] = useState(true)
 
-  useEffect(() => {
-    console.log("file: App.jsx:12  begin:", begin)
-  }, [begin])
+
   return (
     <div className="App">
-      <Splash
-        style={!begin && { display: "none" }}
-        onclick={() => setBegin(false)}
-      />
+      <Splash/>
     </div>
   )
 }
