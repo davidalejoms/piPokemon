3d8c6e126b4c439066d8a009f4d462503bb7df19
davidalejoms <davidalejoms@gmail.com>
2023-07-25 10:27:30 -0500

    'fine adjustments fo splash'

--- client/src/components/Splash/Splash.jsx
+++ client/src/components/Splash/Splash.jsx
@@ -1,21 +1,24 @@
-import { splash, title, button } from "./Splash.module.css"
-import { useEffect, useState } from "react"
-import walking_ash_and_pikachu from '../../assets/walking_ash_and_pikachu.gif'
+import { splash, title, button,contentSplash } from "./Splash.module.css"
+import walking_ash_and_pikachu from "../../assets/walking_ash_and_pikachu.gif"
 import { Link } from "react-router-dom"
 const Splash = () => {
-  
-  const [begin, setBegin] = useState(true)
-  const beginToogler=()=> setBegin(!begin)
-  
-  useEffect(() => {}, [begin])
   return (
     <>
-      <div className={splash} style={begin && {display:'non'}}>
-        <h1 className={title}>Pokemon Api <br /> By David Mejia</h1>
-        <img src={walking_ash_and_pikachu} alt="" />
-    <Link to='/pokemon'>
-        <button className={button} onClick={beginToogler}>Entrar</button>
-    </Link>
+      <div className={splash}>
+
+        <div className={contentSplash}>
+          <h1 className={title}>
+            Pokemon Api <br /> By David Mejia
+          </h1>
+          <img
+            src={walking_ash_and_pikachu}
+            alt=""
+            style={{ width: "100%" }}
+          />
+          <Link to="/pokemon">
+            <button className={button}>Entrar</button>
+          </Link>
+        </div>
       </div>
     </>
   )
